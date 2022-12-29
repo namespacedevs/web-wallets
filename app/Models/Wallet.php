6e77ed3e1700c939cc8ae30042d3f0203eb20078
uigor_fonseca@bobby.com.br <uigor.fonseca@bobby.com.br>
2022-12-28 23:32:39 -0300

    feat: implementa recursos de banco do Wallet

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Wallet extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+    ];
 }
--- database/migrations/2022_12_29_012246_create_wallet.php
+++ database/migrations/2022_12_29_012246_create_wallet.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWallet extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,7 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('wallets');
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('wallet');
     }
 }
